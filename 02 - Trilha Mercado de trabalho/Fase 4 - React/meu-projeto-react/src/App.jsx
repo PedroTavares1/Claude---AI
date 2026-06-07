12a9d4e21a0457121ff22b63024661e80f3f2049
Pedro Tavares <pedroo.25@hotmail.com>
2026-06-07 20:25:48 -0300

    Organização
    Organização, comentários e dúvidas.

--- 02 - Trilha Mercado de trabalho/Fase 4 - React/meu-projeto-react/src/App.jsx
+++ 02 - Trilha Mercado de trabalho/Fase 4 - React/meu-projeto-react/src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import './App.css'
 
+// funções → cuidam dos dados
+// return → cuida do visual
+
 function ListaDeTarefas() {
   const [tarefas, setTarefas] = useState([]) //Lista de tarefas
   const [novaTarefa, setNovaTarefa] = useState('') //Texto da nova tarefa
@@ -13,13 +16,14 @@ function ListaDeTarefas() {
       setNovaTarefa('') // Limpa o campo de entrada após adicionar a tarefa
     }
   }
-  function deletarTarefa(index) {
-    setTarefas(tarefas.filter((_, i) => i !== index))
-  }
+  function deletarTarefa(index) { 
+    setTarefas(tarefas.filter((_, i) => i !== index)) // Remove a tarefa com o índice correspondente
+  } // espaço vazio para ignorar o parâmetro do filtro, já que não é necessário
+
   function concluirTarefa(index) {
-    setTarefas(tarefas.map((tarefa, i) => 
-        i === index ? { ...tarefa, concluida: !tarefa.concluida } : tarefa
-    ))
+    setTarefas(tarefas.map((tarefa, i) => // Percorre a lista de tarefas. Diferente do filter que remove itens, o map transforma itens — percorre a lista e retorna uma lista nova modificada!
+        i === index ? { ...tarefa, concluida: !tarefa.concluida } : tarefa // Se o índice da tarefa for igual ao índice clicado, retorna uma nova tarefa com a propriedade 'concluida' invertida. Caso contrário, retorna a tarefa original sem alterações.
+    )) //
   }
 
   return (
